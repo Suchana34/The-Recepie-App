c72f5f5d5b55efbd500acbb9559662b9e6c14c9c
suchana <suchanachakraborty@gmail.com>
2020-03-25 03:58:34 +0530

    API config file

---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+async function getResult(query){
+    //fetch doesnt work in case of old browsers so we r going to use axios (a http library)
+    //fetch('URL'); axios automatically returns in json format
+    axios();
+}
+getResult();
